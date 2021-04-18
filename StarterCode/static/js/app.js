54c19dfbd67070e939e30f735eaf45f03261acd6
Candi Shanks <candishanks@gmail.com>
2021-04-17 22:43:14 -0400

    adding in drop down values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,53 @@
 
 // Using D3 to import data
-3.json("data/samples.json").then((importedData) => {
+d3.json("data/samples.json").then(data => {
+  console.log(data);
 
-    // holding the data in the variable
-    var data = importedData; 
-
+    // filter samples to element 940
+  
+    let mySelect = d3.select("#selDataset");
+    
+    data.names.forEach(element => {
+      mySelect.append("option").attr("value", element).text(element);
+      
+    })
 
 })
 
+
+// Bar Chart
+// * Use `sample_values` as the values for the bar chart.
+// * Use `otu_ids` as the labels for the bar chart.
+// * Use `otu_labels` as the hovertext for the chart.
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var trace1 = {
+//     x: ,
+//     y: ,
+//     mode: 'markers',
+//     marker: {
+//       size: [40, 60, 80, 100]
+//     }
+//   };
+  
+//   var data = [trace1];
+  
+//   var layout = {
+//     title: 'Marker Size',
+//     showlegend: false,
+//     height: 600,
+//     width: 600
+//   };
+  
+//   Plotly.newPlot('myDiv', data, layout);
